f6c7fbbb59df53a7c3adfec343f4aa7f1e879d1a
Mr. Guo <474224024@qq.com>
2017-05-29 17:42:27 +0800

    更新首页头部

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="index">
-    <header class="eleme-header" style="width: 100%; top: 0px; z-index: 1000;">
-      <div @click="gotoSearch">
-        <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#search"></use></svg>
+    <header class="index-3O8rT">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+           style="position:absolute;width:0;height:0">
+        <defs>
+          <symbol viewBox="0 0 26 31" id="location">
+            <path fill="#FFF" fill-rule="evenodd"
+                  d="M22.116 22.601c-2.329 2.804-7.669 7.827-7.669 7.827-.799.762-2.094.763-2.897-.008 0 0-5.26-4.97-7.643-7.796C1.524 19.8 0 16.89 0 13.194 0 5.908 5.82 0 13 0s13 5.907 13 13.195c0 3.682-1.554 6.602-3.884 9.406zM18 13a5 5 0 1 0-10 0 5 5 0 0 0 10 0z"></path>
+          </symbol>
+        </defs>
+      </svg>
+      <div class="index-MAORp">
+        <div class="index-3vsmj">
+          <svg class="index-3guVd"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#location"></use>
+          </svg>
+          <span class="index-1cnKa">龙华区民治大道(沙元埔民治大道南)</span>
+          <svg class="index-9eIfV"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"></use>
+          </svg>
+        </div>
+        <aside v-if="weather != null" class="index-2LvmP">
+          <div>
+            <h2 class="index-17uRU">{{ weather.temperature }}°</h2>
+            <p class="index-3-P-K">{{ weather.description }}</p>
+          </div>
+          <img alt="天气图标" class="index-wRPUE" :src="weather.image_hash | transformImgUrl(1) + '?imageMogr/format/webp/thumbnail/!69x69r/gravity/Center/crop/69x69/'">
+        </aside>
       </div>
-      <div class="center">
-        <router-link :to="{path: '/city'}">
-          <span>{{ name || '上海商学院(奉浦校区)' }}</span>
-        </router-link>
+      <a><input type="text" placeholder="搜索商家、商品" aria-label="搜索商家、商品" class="index-20Oji"></a>
+      <div v-if="hotSearchWords.length !== 0" class="index-6hVEQ">
+        <template v-for="(item, index) in hotSearchWords">
+          <a>{{ item.word }}</a>
+        </template>
       </div>
-      <div>登录</div>
     </header>
     <my-swipe></my-swipe>
     <h3 class="index-title">
@@ -28,46 +50,102 @@
 </template>
 <style scoped>
   /*  头部样式-start  */
-  .eleme-header {
+  .index-3O8rT {
     box-sizing: border-box;
-    position: relative;
-    padding: 0 .4rem;
-    background-color: #3190e8;
+    padding: .266667rem .373333rem;
+    background-color: #0096ff;
     color: #fff;
-    text-align: center;
-    line-height: 1.173333rem;
-    font-size: .466667rem;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
+    height: 3.253333rem;
+  }
+  .index-MAORp {
     -webkit-box-pack: justify;
     -webkit-justify-content: space-between;
     -ms-flex-pack: justify;
     justify-content: space-between;
+    height: .92rem;
   }
-  .eleme-header a {
-    color: #fff;
-    padding: 0 .4rem;
+  .index-3vsmj, .index-MAORp {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
   }
-  .eleme-header svg {
+  .index-3vsmj {
+    width: 60%;
+  }
+  .index-3guVd {
+    width: .346667rem;
+    height: .413333rem;
     fill: #fff;
-    width: .4rem;
-    height: .44rem;
   }
-  .eleme-header .center {
-    position: absolute;
-    left: 50%;
-    top: 0;
-    width: 50%;
-    margin-left: -25%;
-  }
-  .eleme-header, .eleme-header .center {
-    overflow: hidden;
-    text-overflow: ellipsis;
+  .index-1cnKa {
+    margin: 0 .133333rem;
+    font-size: .48rem;
+    max-width: 80%;
     white-space: nowrap;
+    overflow: hidden;
+  }
+  .index-9eIfV {
+    width: .186667rem;
+    height: .093333rem;
+    fill: #fff;
+  }
+  .index-2LvmP {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+  }
+  .index-17uRU {
+    font-size: .373333rem;
+  }
+  .index-3-P-K {
+    font-size: .266667rem;
+  }
+  .index-wRPUE {
+    margin-left: .106667rem;
+    width: .733333rem;
+    height: .733333rem;
+  }
+  .index-20Oji {
+    display: block;
+    margin: .2rem 0;
+    width: 100%;
+    height: .96rem;
+    text-align: center;
+    border-radius: .96rem;
+    box-shadow: 0 0.026667rem 0.066667rem 0 rgba(0,0,0,.2);
+    color: #333;
+    font-size: .346667rem;
+  }
+  .index-6hVEQ {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    white-space: nowrap;
+    overflow-x: auto;
+  }
+  .index-6hVEQ a {
+    color: currentColor;
+  }
+  .index-6hVEQ a:not(:last-child) {
+    margin-right: .4rem;
   }
   /*  头部样式-end  */
+
   /*  列表标题样式-start  */
   .index-title {
     margin-top: .266667rem;
@@ -112,7 +190,9 @@
         busy: false, // 网络请求状态
         showBack: false, // 是否显示回到顶部按钮
         scrollHeight: 0, // 当前滚动高度
-        timer: null // 滚动时设定的定时器
+        timer: null, // 滚动时设定的定时器
+        weather: null, // 天气
+        hotSearchWords: [] // 热搜词
       }
     },
     computed: {
@@ -149,6 +229,28 @@
           this.busy = false
         })
       },
+      getWeather () {
+        var params = {
+          action: 'weather'
+        }
+        params.latitude = this.latitude
+        params.longitude = this.longitude
+        this.$http({url: 'eleme_api.php', params: params}).then(function (res) {
+          console.log(res)
+          this.weather = res.data
+        })
+      },
+      getHotSearchWords () {
+        var params = {
+          action: 'hot_search_words'
+        }
+        params.latitude = this.latitude
+        params.longitude = this.longitude
+        this.$http({url: 'eleme_api.php', params: params}).then(function (res) {
+          console.log(res)
+          this.hotSearchWords = res.data
+        })
+      },
       gotoSearch(){ //  跳转搜素界面
         this.$router.push({
           name: 'search',
@@ -166,6 +268,8 @@
       }
     },
     created () {
+      this.getWeather()
+      this.getHotSearchWords()
       window.addEventListener('scroll', this.scrollHandler)
     },
     mounted () {
